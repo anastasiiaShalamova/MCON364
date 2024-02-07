25a6b26aa4fb38bec5a1a6535252d9e642cd24cd
anastasiiaShalamova <113482716+anastasiiaShalamova@users.noreply.github.com>
2024-02-06 21:55:14 -0500

    Update Student.java

--- HW1 - Sorting/Student.java
+++ HW1 - Sorting/Student.java
@@ -3,11 +3,13 @@ class Student implements Comparable<Student> {
     private String name;
     private int grade;
 
+   // constructor
     public Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
     }
 
+    // comparable
     @Override
     public int compareTo(Student other) {
         return Integer.compare(this.grade, other.grade);
