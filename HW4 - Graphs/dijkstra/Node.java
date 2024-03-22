cda2f0585ff12840dbd8634a6916ab4d6b9d2a99
anastasiiaShalamova <113482716+anastasiiaShalamova@users.noreply.github.com>
2024-03-22 14:56:45 -0400

    Update Node.java

--- HW4 - Graphs/dijkstra/Node.java
+++ HW4 - Graphs/dijkstra/Node.java
@@ -1,1 +1,65 @@
+import java.util.*;
+
+public class Node {
+
+    private String name;
+
+    // shortest path
+    private List<Node> shortestPath = new ArrayList<>();
+
+    // infiniting distance simulation to other node
+    private Integer distance = Integer.MAX_VALUE;
+
+    // associating adjacent nodes with edge length
+    Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public void addDestination(Node destination, int distance) {
+        adjacentNodes.put(destination, distance);
+    }
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Node> getShortestPath() {
+        return shortestPath;
+    }
+
+    public void setShortestPath(List<Node> shortestPath) {
+        this.shortestPath = shortestPath;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Map<Node, Integer> getAdjacentNodes() {
+        return adjacentNodes;
+    }
+
+    public void setAdjacentNodes(Map<Node, Integer> adjacentNodes) {
+        this.adjacentNodes = adjacentNodes;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "name='" + name + '\'' +
+                ", shortestPath=" + shortestPath +
+                ", distance=" + distance +
+                '}';
+    }
+}
 
