287a4f4d7c7d8c0234931ba09184168c62c31faa
anastasiiaShalamova <113482716+anastasiiaShalamova@users.noreply.github.com>
2024-05-02 18:27:07 -0400

    Update Main.java

--- Calculator JavaFX/Main.java
+++ Calculator JavaFX/Main.java
@@ -1,1 +1,31 @@
+package Calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("C:\\Users\\student\\eclipse-workspace\\MCONJFXTest\\src\\Calculator\\main.fxml"));
+			Scene mainScene = new Scene(root,300,350);
+			primaryStage.setTitle("Demo CalculatorT");
+			primaryStage.setScene(mainScene);
+			primaryStage.show();
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
 
